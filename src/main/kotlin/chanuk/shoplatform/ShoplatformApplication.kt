6d083b5bca2f92d0c6aed210a190b2d938330e22
trueor <wisdomichanuk@gmail.com>
2025-03-23 22:05:35 +0900

    초기 프로젝트 등록

---
+++ src/main/kotlin/chanuk/shoplatform/ShoplatformApplication.kt
@@ -0,0 +1,11 @@
+package chanuk.shoplatform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoplatformApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShoplatformApplication>(*args)
+}
---
+++ src/test/kotlin/chanuk/shoplatform/ShoplatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package chanuk.shoplatform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoplatformApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
